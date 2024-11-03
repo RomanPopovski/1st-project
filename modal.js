0375d7b1679c03519f6814211678a5071d8fd9a2
RomanPopovski <runway963@gmail.com>
2024-11-03 11:16:05 +0100

    Added responsibility

--- modal.js
+++ modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnOpen1 = document.querySelector(".modal-btn-open1");
 const modalBtnClose = document.querySelector(".modal__btn-close");
 
 const toggleModal = () => modal.classList.toggle("is-hidden");
 
 modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnOpen1.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
